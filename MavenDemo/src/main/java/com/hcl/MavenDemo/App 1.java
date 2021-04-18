9db3cd45f42525efe63e019355cc57c91710a8e7
kallurusushma <kallurusushmareddy@gmail.com>
2021-04-18 19:54:22 +0530

    new

---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App 1.java
@@ -0,0 +1,15 @@
+package com.hcl.MavenDemo;
+
+import com.hcl.MavenDemo.maths.MathUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1
+{
+    public static void m1()
+    {
+        System.out.println( "m1 call" );        
+    }
+}
