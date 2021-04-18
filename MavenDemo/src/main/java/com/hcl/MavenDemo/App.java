8fda0a98cfa870f5dff4b0175e919e8b4565a69b
kallurusushma <kallurusushmareddy@gmail.com>
2021-04-18 16:34:41 +0530

    first commit

---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App.java
@@ -0,0 +1,17 @@
+package com.hcl.MavenDemo;
+
+import com.hcl.MavenDemo.maths.MathUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("A Very Good Morning");
+        System.out.println(MathUtil.factorial(5));
+    }
+}
---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/maths/MathUtil.java
@@ -0,0 +1,12 @@
+package com.hcl.MavenDemo.maths;
+
+public class MathUtil {
+public static int factorial(int n)
+{
+	int f=1;
+	for(int i=1;i<=n;i++)
+		f*=i;
+	return f;
+	
+}
+}
